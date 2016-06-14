472fc1b20aa55b04ac142c2088bcfb5823237ddf
Robert Luong <robert.luong@gmail.com>
2016-06-14 03:15:43 +0100

    Integrated redux and constant-ised the action types.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
-import { createCourse } from '../../actions/courseActions';
+import * as courseActions from '../../actions/courseActions';
+import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -21,7 +22,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
   }
 
   courseRow(course, index) {
@@ -49,7 +50,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  createCourse: PropTypes.func.isRequired
+  actions: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -60,7 +61,7 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    createCourse: course => dispatch(createCourse(course))
+    actions: bindActionCreators(courseActions, dispatch)
   };
 }
 
